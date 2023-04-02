ecbe1eb5c04a9b1be86e7930f316beede9f34732
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-02 15:28:59 +0100

    Project initialization & database creation

---
+++ lib/phx_chat/application.ex
@@ -0,0 +1,38 @@
+defmodule PhxChat.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      PhxChatWeb.Telemetry,
+      # Start the Ecto repository
+      PhxChat.Repo,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: PhxChat.PubSub},
+      # Start Finch
+      {Finch, name: PhxChat.Finch},
+      # Start the Endpoint (http/https)
+      PhxChatWeb.Endpoint
+      # Start a worker by calling: PhxChat.Worker.start_link(arg)
+      # {PhxChat.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: PhxChat.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    PhxChatWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
